a0104c27d9aa188c436cff8d4b640ed2ec529e66
Braedennnnn <149883938+Braedennnnn@users.noreply.github.com>
2024-02-05 14:51:09 -0600

    Update StudentManager.java
    Fixed my idiocity

--- Homework_2/src/homework2/StudentManager.java
+++ Homework_2/src/homework2/StudentManager.java
@@ -1,11 +1,23 @@
 package homework2;
 
+import java.util.Scanner;
+
+
 public class StudentManager {
 
-private ArrayList<Student> students;
+private Student[] students;
 
+public boolean readFromFile(String filename){
 
+File file = new File(filename);
 
+Scanner sc = new Scanner(file);
+
+while (sc.hasNextLine()){
+    
+}
+
+}
 
 
 
