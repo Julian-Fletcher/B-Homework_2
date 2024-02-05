09ec33c65eae798b8dd956b5dc90ee133f3a6391
Julian Fletcher <37055413+Julian-Fletcher@users.noreply.github.com>
2024-02-05 10:21:07 -0600

    Create initial file
    Created classes and main. Created Student class with getters/setters and toString

---
+++ Homework_2/src/homework2/Main.java
@@ -0,0 +1,28 @@
+package homework2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Output!");
+		/*
+		// Instantiate StudentManager, perform operations based on the requirements.
+		StudentManager studentManager = new StudentManager();
+		
+		// Read student data from a file and initialize Student objects.
+		boolean fileReadStatus = studentManager.readFromFile("studentData.txt");
+		
+		// Display all students.
+		studentManager.displayStudents();
+		
+		// Search for a student by ID.
+		boolean studentFound = studentManager.searchStudentById(101);
+		
+		// Update the grade of a student by ID.
+		boolean studentGradeUpdateStatus = studentManager.updateStudentGradeById(102, 95);
+		
+		// Display all students after the update.
+		studentManager.displayStudents();
+		*/
+	}
+
+}
