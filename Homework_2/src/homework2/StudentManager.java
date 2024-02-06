0d35d53e81c555ad4b7c046f694ebf80e01f9a33
Braedennnnn <149883938+Braedennnnn@users.noreply.github.com>
2024-02-06 11:18:11 -0600

    Student Manager Updates
    Added StudentManager init function as well as added ReadFromFile function

--- Homework_2/src/homework2/StudentManager.java
+++ Homework_2/src/homework2/StudentManager.java
@@ -1,20 +1,54 @@
 package homework2;
 
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 
 public class StudentManager {
 
 private Student[] students;
 
+
+
+public StudentManager() {
+	
+	this.students = new Student[10];
+	
+}
+
+
+
 public boolean readFromFile(String filename){
 
 File file = new File(filename);
 
-Scanner sc = new Scanner(file);
+Scanner scanner;
+try {
+	scanner = new Scanner(file);
+	int count = 0;
+	while (scanner.hasNextLine()){
+		
+		Student temp = new Student();
+		int id = scanner.nextInt();
+		String name = scanner.next();
+		double grade = scanner.nextDouble();
+	    temp.setId(id);
+	    temp.setName(name);
+	    temp.setGrade(grade);
+	    students[count] = temp;
+	    count++;
+	}
+	
+	scanner.close();
+	return true;
+} catch (FileNotFoundException e) {
+	e.printStackTrace();
+	return false;
+}
 
-while (sc.hasNextLine()){
-    
+
+
 }
 
 }
